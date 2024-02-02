0380a59d99b2a35db38743ec0da7850487fff0ed
silviuavramut <silviu.avramut@randstaddigital.com>
2024-02-02 13:15:00 +0200

    Service finished
    Functions for the moment have hardcoded results, no logic

--- changes_service.mk
+++ changes_service.mk
@@ -1,5 +1,8 @@
 PRODUCT_PACKAGES += devtempstats_service
 
+BOARD_SEPOLICY_DIRS += device/stats_silviu/sepolicy \
+                       device/stats_silviu/sepolicy/example_service
+
 LOCAL_SHARED_LIBRARIES += devtempstats.service.api
 
 DEVICE_MANIFEST_FILE += device/stats_silviu/manifests/manifest.xml
--- devtempstats_service/native/src/impl/DevTempStatsService.cpp
+++ devtempstats_service/native/src/impl/DevTempStatsService.cpp
@@ -30,7 +30,95 @@ DevTempStatsService::~DevTempStatsService()
 
 }
 
+::ndk::ScopedAStatus DevTempStatsService::getCpuTemperature(float* _aidl_return)
+{
+   float cpuTemp;
+   
+   cpuTemp = 10.0;
 
+   *_aidl_return = cpuTemp;
+   return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus DevTempStatsService::getGpuTemperature(float* _aidl_return)
+{
+   float gpuTemp;
+   
+   gpuTemp = 10.0;
+
+   *_aidl_return = gpuTemp;
+   return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus DevTempStatsService::getAmbientTemperature(float* _aidl_return)
+{
+   float ambientTemp;
+   
+   ambientTemp = 10.0;
+
+   *_aidl_return = ambientTemp;
+   return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus DevTempStatsService::getAverageCpuTemperature(float* _aidl_return)
+{
+   float cpuTemp;
+   
+   cpuTemp = 20.0;
+
+   *_aidl_return = cpuTemp;
+   return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus DevTempStatsService::getAverageGpuTemperature(float* _aidl_return)
+{
+   float gpuTemp;
+   
+   gpuTemp = 20.0;
+
+   *_aidl_return = gpuTemp;
+   return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus DevTempStatsService::getAverageAmbientTemperature(float* _aidl_return)
+{
+   float ambientTemp;
+   
+   ambientTemp = 20.0;
+
+   *_aidl_return = ambientTemp;
+   return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus DevTempStatsService::getMaxCpuTemperature(float* _aidl_return)
+{
+   float cpuTemp;
+   
+   cpuTemp = 30.0;
+
+   *_aidl_return = cpuTemp;
+   return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus DevTempStatsService::getMaxGpuTemperature(float* _aidl_return)
+{
+   float gpuTemp;
+   
+   gpuTemp = 30.0;
+
+   *_aidl_return = gpuTemp;
+   return ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus DevTempStatsService::getMaxAmbientTemperature(float* _aidl_return)
+{
+   float ambientTemp;
+   
+   ambientTemp = 30.0;
+
+   *_aidl_return = ambientTemp;
+   return ndk::ScopedAStatus::ok();
+}
 
 
 }
